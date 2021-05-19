59758b384f03193c4709931e770aa63ae2e77f35
01100010 <jiahao.e21@gmail.com>
2021-05-19 10:33:34 +0200

    Finished tests

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -14,6 +15,7 @@ class CloudStorageApplicationTests {
 	private int port;
 
 	private WebDriver driver;
+	private WebDriverWait driverWait;
 
 	@BeforeAll
 	static void beforeAll() {
@@ -23,6 +25,7 @@ class CloudStorageApplicationTests {
 	@BeforeEach
 	public void beforeEach() {
 		this.driver = new ChromeDriver();
+		this.driverWait = new WebDriverWait (this.driver, 1000);
 	}
 
 	@AfterEach
@@ -39,6 +42,7 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
+	@Order(1)
 	public void testSignupLoginFlow(){
 
 		driver.get("http://localhost:" + this.port + "/signup");
@@ -50,6 +54,50 @@ class CloudStorageApplicationTests {
 
 		Assertions.assertEquals("Home", driver.getTitle());
 
+	}
+
+	@Test
+	@Order(2)
+	public void testCRUDCredentialsFlow(){
+		String baseURI = "http://localhost:" + this.port;
+
+		//login
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("hello","alt12345");
+
+		Assertions.assertEquals("Home", driver.getTitle());
+
+		//Test credentials
+		HomePage homePage = new HomePage(driver,driverWait);
+		driver.get(baseURI + "/home#nav-credentials-tab");
+		homePage.createCredential("http://localhost:8080","user","123");
+		driver.get(baseURI + "/home#nav-credentials-tab");
+		homePage.editCredential("username2");
+		driver.get(baseURI + "/home#nav-credentials-tab");
+		homePage.deleteCredential();
+
+	}
+
+	@Test
+	@Order(3)
+	public void testCRUDNotesFlow(){
+		String baseURI = "http://localhost:" + this.port;
+		//login
+		driver.get("http://localhost:" + this.port + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("hello","alt12345");
+
+		Assertions.assertEquals("Home", driver.getTitle());
+
+		//Test notes
+		HomePage homePage = new HomePage(driver,driverWait);
+		driver.get(baseURI + "/home#nav-notes-tab");
+		homePage.createNote("TFG","descripcion corta");
+		driver.get(baseURI + "/home#nav-notes-tab");
+		homePage.editNote("second description");
+		driver.get(baseURI + "/home#nav-notes-tab");
+		homePage.deleteNote();
 
 	}
 
---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -0,0 +1,149 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+  @FindBy(id = "nav-notes-tab")
+  private WebElement notesTab;
+
+  @FindBy(id = "add-note-btn")
+  private WebElement addNoteBtn;
+
+  @FindBy(id = "note-title")
+  private WebElement noteTitle;
+
+  @FindBy(id = "note-description")
+  private WebElement noteDescription;
+
+  @FindBy(id = "note-save")
+  private WebElement noteSave;
+
+  @FindBy(xpath = "//*[@id=\"userTable\"]/tbody/tr/td[1]/button")
+  private WebElement noteEditBtn;
+
+  @FindBy(xpath = "//*[@id=\"userTable\"]/tbody/tr/td[1]/a")
+  private WebElement noteDelBtn;
+
+  @FindBy(id = "nav-credentials-tab")
+  private WebElement credentialsTab;
+
+  @FindBy(id = "credential-add-btn")
+  private WebElement credentialAddBtn;
+
+  @FindBy(id = "credential-url")
+  private WebElement credentialUrl;
+
+  @FindBy(id = "credential-username")
+  private WebElement credentialUsername;
+
+  @FindBy(id = "credential-password")
+  private WebElement credentialPassword;
+
+  @FindBy(id = "credential-save")
+  private WebElement credentialSave;
+
+  @FindBy(xpath = "//*[@id=\"credentialTable\"]/tbody/tr/td[1]/a")
+  private WebElement deleteCredentialBtn;
+
+  @FindBy(xpath = "//*[@id=\"credentialTable\"]/tbody/tr/td[1]/button")
+  private WebElement editCredentialBtn;
+
+  @FindBy(id = "submit-button")
+  private WebElement submitButton;
+
+  private WebDriver driver;
+  private final WebDriverWait driverWait;
+  private final JavascriptExecutor js;
+
+  public HomePage(WebDriver webDriver,WebDriverWait webDriverWait) {
+    PageFactory.initElements(webDriver, this);
+    this.driver = webDriver;
+    this.driverWait = webDriverWait;
+    this.js = (JavascriptExecutor) this.driver;
+  }
+
+  public void createCredential(String url,String user,String password){
+    this.js.executeScript("arguments[0].click()",this.credentialsTab);
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.credentialAddBtn));
+    this.credentialAddBtn.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.credentialUrl));
+    this.credentialUrl.sendKeys(url);
+    this.credentialUsername.sendKeys(user);
+    this.credentialPassword.sendKeys(password);
+    this.credentialSave.click();
+  }
+
+  public void editCredential(String username){
+    this.credentialsTab.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.editCredentialBtn));
+    this.editCredentialBtn.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.credentialUsername));
+    this.credentialUsername.clear();
+    this.credentialUsername.sendKeys(username);
+    this.credentialSave.click();
+
+  }
+
+  public void deleteCredential(){
+    this.credentialsTab.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.deleteCredentialBtn));
+    this.deleteCredentialBtn.click();
+  }
+
+
+  public void createNote(String title, String description){
+    this.js.executeScript("arguments[0].click()",this.notesTab);
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.addNoteBtn));
+    this.addNoteBtn.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.noteTitle));
+    this.noteTitle.sendKeys(title);
+    this.noteDescription.sendKeys(description);
+    this.noteSave.click();
+  }
+
+  public void editNote(String description){
+    this.notesTab.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.noteEditBtn));
+    this.noteEditBtn.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.noteDescription));
+    this.noteDescription.clear();
+    this.noteDescription.sendKeys(description);
+    this.noteSave.click();
+  }
+
+  public void deleteNote(){
+    this.notesTab.click();
+    this.driverWait.until(ExpectedConditions.elementToBeClickable(this.noteDelBtn));
+    this.noteDelBtn.click();
+  }
+
+
+
+
+/*
+  public Note getFirstNote() {
+    String title = wait.until(ExpectedConditions.elementToBeClickable(tableNoteTitle)).getText();
+    String description = tableNoteDescription.getText();
+
+    return new Note(title, description);
+  }
+
+  public Credential getFirstCredential() {
+    String url = wait.until(ExpectedConditions.elementToBeClickable(tblCredentialUrl)).getText();
+    String username = tblCredentialUsername.getText();
+    String password = tblCredentialPassword.getText();
+
+    return new Credential(url, username, password);
+  }
+
+ */
+}
